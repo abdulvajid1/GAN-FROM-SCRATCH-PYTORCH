548451d5c2f4a37a7c1c539cd55efde6407f1efe
abdulvajid1 <vajidca444@gmail.com>
2025-08-03 17:19:29 +0530

    DCGAN model Skelton Structure

---
+++ model.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from typing import Annotated
+
+
+class Generator(nn.Module):
+    """Generate new image from input sampled from normal distribution.
+    """
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def forward(self, x):
+        pass
+    
+class Descriminator(nn.Module):
+    """
+    Classify image => real or fake
+    """
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def forward(self, img, labels) -> Annotated[(list, float), "()"]:
+        pass
+    
+class DCGAN(nn.Module):
+    """
+    GAN model
+    """
+    def __init__(self,generator: Generator, descriminator: Descriminator, batch_size, device):
+        super().__init__()
+        self.generator = generator
+        self.descrimator = descriminator
+        self.batch_size = batch_size
+        self.device = device
+        pass
+    
+    def generate_image_batch(self, batch_size):
+        pass
+    
+    def descriminate_batch(self, img_batch):
+        pass 
+    
+    def forward(self, x):
+        fake_img_batch = self.generate_image_batch(self.batch_size) # (B, C, W, H)
+        real_img_batch = x # real image comes from the dataset
+        
+        real_labels = torch.ones(size=(self.batch_size, ), device=self.device)
+        fake_labels = torch.zeros(size=(self.batch_size, ), device=self.device)
+        
+        # Descriminate
+        real_desc_pred = self.descrimator(real_img_batch, real_labels)
+        fake_desc_pred = self.descrimator(fake_img_batch, fake_labels) # Descrimating Generated image
+        
+        # Descriminator
+        real_desc_loss = F.binary_cross_entropy(real_desc_pred, real_labels)
+        fake_desc_loss = F.binary_cross_entropy(fake_desc_loss, fake_labels)
+        descriminator_loss = (real_desc_loss + fake_desc_loss) / 2.0 # average
+        
+        # Generator loss
+        generator_loss = F.binary_cross_entropy(fake_desc_pred, real_labels)
+        
+        return generator_loss, descriminator_loss
+        
+        
+    
