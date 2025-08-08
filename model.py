3aa4ffbafe2358e30978e8b32a5877fde85eba21
abdulvajid1 <vajidca444@gmail.com>
2025-08-08 09:14:17 +0530

    train code

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2), #  (64, 10, 10)
             nn.BatchNorm2d(num_features=64),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2, output_padding=1), # (32, 22, 22)
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2), # (32, 22, 22)
             nn.BatchNorm2d(num_features=32),
             nn.LeakyReLU(),
             nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=4, stride=1), # (16, 25, 25)
@@ -31,7 +31,7 @@ class Generator(nn.Module):
         
     
     def forward(self, batch_size):
-        latent_inp = torch.rand_like((batch_size, self.latent_dim)) # (b, h)
+        latent_inp = torch.randn(size=(batch_size, self.latent_dim)) # (b, h)
         x = torch.unsqueeze(torch.unsqueeze(latent_inp, dim=-1), dim=-1) # (b, h, 1, 1)
         return self.seq_pipe(x)
         
@@ -47,7 +47,7 @@ class Descriminator(nn.Module):
         self.descriminator = nn.Sequential(
             
             # (inp + 1)/stride + 2*padding - kernal
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding='same'), # (3, 28, 28) -> (64, 28, 28)
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding='same'), # (3, 28, 28) -> (64, 28, 28)
             nn.BatchNorm2d(num_features=64),
             nn.LeakyReLU(),
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2), # (128, 17, 17)
@@ -65,7 +65,8 @@ class Descriminator(nn.Module):
             nn.Sigmoid()
         )
     
-    def forward(self, img_batch, labels) -> Annotated[(list, float), "()"]:
+    def forward(self, img_batch):
+        assert img_batch.shape[1:] == torch.Size([1, 28, 28]), 'the size of the image should (b, 1, 28, 28)' 
         predictions = self.descriminator(img_batch)
         return predictions
         
@@ -80,13 +81,12 @@ class DCGAN(nn.Module):
         self.descrimator = descriminator
         self.batch_size = batch_size
         self.device = device
-        pass
     
     def generate_image_batch(self, batch_size):
-        pass
+        return self.generator(batch_size)
     
     def descriminate_batch(self, img_batch):
-        pass 
+        return self.descriminator(img_batch)
     
     def forward(self, x):
         fake_img_batch = self.generate_image_batch(self.batch_size) # (B, C, W, H)
@@ -96,18 +96,23 @@ class DCGAN(nn.Module):
         fake_labels = torch.zeros(size=(self.batch_size, ), device=self.device)
         
         # Descriminate
-        real_desc_pred = self.descrimator(real_img_batch)
-        fake_desc_pred = self.descrimator(fake_img_batch) # Descrimating Generated image
+        real_desc_pred = self.descrimator(real_img_batch).squeeze() # (5, 1)
+        fake_desc_pred = self.descrimator(fake_img_batch).squeeze() # Descrimating Generated image
         
         # Descriminator
         real_desc_loss = F.binary_cross_entropy(real_desc_pred, real_labels)
-        fake_desc_loss = F.binary_cross_entropy(fake_desc_loss, fake_labels)
+        fake_desc_loss = F.binary_cross_entropy(fake_desc_pred, fake_labels)
         descriminator_loss = (real_desc_loss + fake_desc_loss) / 2.0 # average
-        
         # Generator loss
         generator_loss = F.binary_cross_entropy(fake_desc_pred, real_labels)
-        
         return generator_loss, descriminator_loss
         
         
+def build_model(latent_dim, batch_size):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    descriminator = Descriminator()
+    generator = Generator(latent_dim=latent_dim)
     
+    model = DCGAN(generator, descriminator, batch_size, device=device)
+    
+    return model
