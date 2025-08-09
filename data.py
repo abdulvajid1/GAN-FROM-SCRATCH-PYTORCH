111ccb4ffe6e0fd9be9def53a9118500d0494205
abdul vajid <vajidca444@gmail.com>
2025-08-09 07:46:05 +0530

    train complete

--- data.py
+++ data.py
@@ -5,11 +5,12 @@ from torch.utils.data import DataLoader
 def load_data(train_batch_size=8, test_batch_size=2):
     
     transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize([0.5], [0.5])
     ])
     
-    train_dataset = FashionMNIST(root='data/', train=True, transform=transform, download=False)
-    test_dataset = FashionMNIST(root='data/', train=False, transform=transform, download=False)
+    train_dataset = FashionMNIST(root='data/', train=True, transform=transform, download=True)
+    test_dataset = FashionMNIST(root='data/', train=False, transform=transform, download=True)
     train_loader = DataLoader(dataset=train_dataset, batch_size=train_batch_size, shuffle=True)
     test_loader = DataLoader(dataset=test_dataset, batch_size=test_batch_size, shuffle=False)
     
--- model.py
+++ model.py
@@ -7,31 +7,32 @@ from typing import Annotated
 class Generator(nn.Module):
     """Generate new image from input sampled from normal distribution.
     """
-    def __init__(self, latent_dim):
+    def __init__(self, latent_dim, device='cuda'):
         super().__init__()
         self.latent_dim = latent_dim
+        self.device = device
         
         # (input - 1) * stride - 2*padding + kernal size 
         self.seq_pipe = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=self.latent_dim, out_channels=128, stride=1, kernel_size=4), # (128, 4, 4)
+            nn.ConvTranspose2d(in_channels=self.latent_dim, out_channels=512, stride=1, kernel_size=4), # (128, 4, 4)
+            nn.BatchNorm2d(num_features=512),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2), #  (64, 10, 10)
+            nn.BatchNorm2d(num_features=256),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2), # (32, 22, 22)
             nn.BatchNorm2d(num_features=128),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2), #  (64, 10, 10)
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=1), # (16, 25, 25)
             nn.BatchNorm2d(num_features=64),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2), # (32, 22, 22)
-            nn.BatchNorm2d(num_features=32),
-            nn.LeakyReLU(),
-            nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=4, stride=1), # (16, 25, 25)
-            nn.BatchNorm2d(num_features=16),
-            nn.LeakyReLU(),
-            nn.ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=4, stride=1), # (1, 28, 28)
-            nn.Sigmoid()
+            nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=4, stride=1), # (1, 28, 28)
+            nn.Tanh()
         )
         
     
     def forward(self, batch_size):
-        latent_inp = torch.randn(size=(batch_size, self.latent_dim)) # (b, h)
+        latent_inp = torch.randn(size=(batch_size, self.latent_dim)).to(self.device) # (b, h)
         x = torch.unsqueeze(torch.unsqueeze(latent_inp, dim=-1), dim=-1) # (b, h, 1, 1)
         return self.seq_pipe(x)
         
